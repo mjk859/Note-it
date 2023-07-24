5471f0a56a3ba14f113d54fb449f3918479507b1
Jaseel <muhammedjaseelk10@gmail.com>
2023-07-24 13:31:55 +0530

    added css and material UI

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
 
 function CreateArea(props) {
     const [note, setNote] = useState({
@@ -23,11 +24,12 @@ function CreateArea(props) {
     
     return(
        
-        <div>
-            <input name="title" onChange={handleChange} placeholder="Enter title" value={note.title}></input>
+        <div className="container">
+            <input type="text" className="styled-input" name="title" onChange={handleChange} placeholder="Enter title" value={note.title}></input>
             <br></br>
-            <input name="content" onChange={handleChange} placeholder="Enter content" value={note.content}></input>
-            <button onClick={handleClick}>+</button>
+            <textarea type="text" className="styled-textArea" name="content" onChange={handleChange} placeholder="Enter content" value={note.content}
+            rows={3}></textarea>
+            <button className="action-button" onClick={handleClick}><AddIcon/></button>
         </div>
         
         
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import StickyNote2Icon from '@mui/icons-material/StickyNote2';
 
 function Header() {
     return(
         <header className="header">
-            <h1>Note it !</h1>
+            
+            <h1><StickyNote2Icon /> Note it !</h1>
         </header>
     )
 }
 
 export default Header
+
+// header
+/* Add padding to create some space around the header content */
+/* Set the background color of the header */
+/* Set the text color of the header */
+
+// header-container (deleted)
+/* Including padding and border in the specified width and height */
+/* Set the width of the container, adjust as needed */
+/* Set the minimum height of the container, adjust as needed */
+/* Set the background color of the container */
+/* Adjust the overflow property as per your requirement */
